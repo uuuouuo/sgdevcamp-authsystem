75b2aa2065ca6f19173bb99c25a167854dc70407
장소명 <som0ng@naver.com>
2022-12-22 17:30:46 +0900

    [refactor] 파일 수정

---
+++ authsystem-backend/src/main/java/com/stdevcamp/authsystembackend/AuthsystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stdevcamp.authsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthsystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthsystemBackendApplication.class, args);
+	}
+
+}
---
+++ authsystem-backend/src/test/java/com/stdevcamp/authsystembackend/AuthsystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stdevcamp.authsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthsystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
